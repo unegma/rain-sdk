97b1ffe26a6c166b4f51bde52a2787f52f88cff5
Timothy Coleman <timhc22@gmail.com>
2022-04-11 18:15:29 +0100

    Add addresses class

---
+++ src/Addresses.ts
@@ -0,0 +1,27 @@
+const addressBook = [
+  {
+    chainId: 8001,
+    addresses: {
+      gatedNFT: '0xD9AD3A87E4c1F604091c1A0E147c88B3A9E1B4ad',
+      sale: '0x7fbc27f1d14e0c59e2f4346b5166692659c4694d',
+    },
+  },
+];
+
+/**
+ * AddressBook
+ */
+export default class AddressBook {
+
+  /**
+   * Get Addresses For Chain Id
+   * @param chainId
+   */
+  public static getAddressesForChainId(chainId: number) {
+    const network = addressBook.find(n => n.chainId === chainId);
+    if (!network?.addresses) {
+      throw new Error('No deployed contracts for this chain.');
+    }
+    return network.addresses;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { default as ExampleClass } from './ExampleClass';
+export { default as Addresses } from './Addresses';
